b7d6eb0f97a9ebfe42f0e0cb13bfe5b0adeab676
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-10-04 14:32:05 +0530

    Scripted auto-commit on change (2022-10-04 14:32:05) by gitwatch.sh

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
 import pyttsx3
-from book import Book
+
 engine = pyttsx3.init()
 
 import PyPDF2
+
 
 class Book:
     """
     Class to extract book pages
     """
-    def __init__(self,name:str) -> None:
+
+    def __init__(self, name: str) -> None:
         """name is the name of the pdf file"""
-        pdfFile = open(name, 'rb')
+        pdfFile = open(name, "rb")
         self.pdfReader = PyPDF2.PdfFileReader(pdfFile)
         self.page = self.pdfReader.numPages
         self.first_page = self.first_page_with_text()
-    
-    def get_page(self,page:int) -> str:
-        return self.pdfReader.getPage(page).extractText().replace("\t"," ")
-    
+
+    def get_page(self, page: int) -> str:
+        return self.pdfReader.getPage(page).extractText().replace("\t", " ")
+
     def first_page_with_text(self) -> int:
         for i in range(self.page):
             pageObj = self.pdfReader.getPage(i)
             if pageObj.extractText().strip():
                 return i
         return -1
-    
-    def extract_book(self)  -> str:
+
+    def extract_book(self) -> str:
         first_page = self.first_page_with_text()
         if first_page == -1:
             return ""
 
-        return "".join(self.get_page(i) for i in range(first_page,self.page))
-            
+        return "".join(self.get_page(i) for i in range(first_page, self.page))
+
 
 book = Book("the-india-way.pdf")
-engine.setProperty('voices', 'com.apple.speech.synthesis.voice.rishi')
-for i in range(book.first_page,book.page):
+engine.setProperty("voices", "com.apple.speech.synthesis.voice.rishi")
+for i in range(book.first_page, book.page):
     engine.say(book.get_page(i))
     engine.runAndWait()
     break
